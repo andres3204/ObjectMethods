2cd0008ebfbd673f2efb1631b1d40f0d9d26ddca
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-06-03 12:48:27 -0500

    Add project files.

---
+++ ObjectMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student1 = new Student("Andres", "Computer Science", 3.6);
+            Student student2 = new Student("Luis", "Business", 3.5);
+            Student student3 = new Student("Sofia", "Art Design", 2.9);
+
+            
+            Console.WriteLine(student1.Name + "," + " who studies " + student1.Major + "," + " has a GPA of " + student1.GPA +"," + " therefore he " + student1.HasHonors());
+            Console.WriteLine(student2.Name + "," + " who studies " + student2.Major + "," + " has a GPA of " + student2.GPA +"," + " therefore he " + student2.HasHonors());
+            Console.WriteLine(student3.Name + "," + " who studies " + student3.Major + "," + " has a GPA of " + student3.GPA +"," + " therefore he " + student3.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ObjectMethods/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethods
+{
+    internal class Student
+    {
+        public string Name;
+        public string Major;
+        public double GPA;
+
+        public Student(string aName, string aMajor, double aGPA)
+        {
+            Name = aName;
+            Major = aMajor;
+            GPA = aGPA;
+        }
+
+        public string HasHonors()
+        {
+            if(GPA >= 3.5)
+            {
+                string result = "does have honors.";
+                return result;
+                
+            }
+            else
+            {
+                string result = "does not have honors.";
+                return result;
+                
+            }
+        }
+
+    }
+}
